9071aef9ff1dd51266c133a03e958091a48d0f3d
rafimd422 <rafimd2222@gmail.com>
2025-07-06 01:06:37 +0600

    feat: implement short_code generation and validations in ShortUrl model

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,17 @@
 class ShortUrl < ApplicationRecord
+  validates :original_url, presence: true, format: URI::regexp(%w[http https])
+  validates :short_code, uniqueness: true
+
+  before_validation :generate_short_code, on: :create
+
+  private
+
+  def generate_short_code
+    return if short_code.present?
+
+    loop do
+      self.short_code = SecureRandom.alphanumeric(6)
+      break unless ShortUrl.exists?(short_code: short_code)
+    end
+  end
 end
